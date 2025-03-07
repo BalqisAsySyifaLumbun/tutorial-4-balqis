a6f936f780fb1825a4a519c525069036196255e5
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2025-03-07 12:13:12 +0700

    Add Spawner

--- scenes/ikan.gd
+++ scenes/ikan.gd
@@ -2,8 +2,13 @@ extends RigidBody2D
 
 @export var sceneName = "LoseScreen"
 
-func _on_FallArea_body_entered(body):
-	if body.get_name() == "Player":
-		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+func _ready():
+	print("Signal Area2D dihubungkan!")  
+	$Area2D.body_entered.connect(_on_Area2D_body_entered)
+
+func _on_Area2D_body_entered(body: Node2D) -> void:
+	print("Objek jatuh:", body.name)  # Debugging
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://scenes/LoseScreen.tscn")
 	else:
-		body.queue_free()
+		body.queue_free()# Replace with function body.
